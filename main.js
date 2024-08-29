477a4d5c2d27cb3933bbf5618edb08dac6362faf
sabbir hossain <mdsabbir477470@gmail.com>
2024-08-29 14:54:06 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+
+// mobile er real password ki vebe kaj kore..
+
+// যখন আমি বাসা থেকে চলে আসি তখন আম্মু বলেছে যে জব পাইতে হবে প্লাস দুই লাখ টাকা আনতে হবে তবেই tor বিয়ে হবে
+
+var jobPiso = false;
+var savingAmount = 500;
+if (jobPiso == true && savingAmount > 200000) {
+    console.log("Assa tor jonno patri khuji");
+} else if (jobPiso == true) {
+    console.log("Assa dake jabe koydin por");
+}
+else {
+    console.log("Tor kopale beya nai");
+}
